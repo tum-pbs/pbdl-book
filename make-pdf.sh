370ee58d4baa3b468ba055b6773a9ad0a007f2ca
N_T <nils.thuerey @ tum.de>
2023-11-14 21:18:45 +0100

    fixed typo, and script update

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,6 +1,7 @@
 # source this file with "." in a shell
 
 # note this script assumes the following paths/versions: python3.7 , /Users/thuerey/Library/Python/3.7/bin/jupyter-book
+# updated for nMBA !
 
 # do clean git checkout for changes from json-cleanup-for-pdf.py via:
 # git checkout diffphys-code-burgers.ipynb diffphys-code-ns.ipynb diffphys-code-sol.ipynb physicalloss-code.ipynb bayesian-code.ipynb supervised-airfoils.ipynb reinflearn-code.ipynb physgrad-code.ipynb physgrad-comparison.ipynb physgrad-hig-code.ipynb
@@ -9,13 +10,17 @@ echo
 echo WARNING - still requires one manual quit of first pdf/latex pass, use shift-x to quit
 echo
 
+PYT=python3.7
+PYT=python3
+
 # warning - modifies notebooks!
-python3.7 json-cleanup-for-pdf.py
+${PYT} json-cleanup-for-pdf.py
 
 # clean / remove _build dir ?
 
 # GEN!
-/Users/thuerey/Library/Python/3.7/bin/jupyter-book build . --builder pdflatex
+#/Users/thuerey/Library/Python/3.7/bin/jupyter-book build . --builder pdflatex
+/Users/thuerey/Library/Python/3.9/bin/jupyter-book build . --builder pdflatex
 
 cd _build/latex
 #mv book.pdf book-xetex.pdf # not necessary, failed anyway
@@ -29,7 +34,7 @@ mv book.aux book-in.aux
 mv book.toc book-in.toc
 #mv sphinxmanual.cls sphinxmanual-in.cls
 
-python3.7 ../../fixup-latex.py
+${PYT} ../../fixup-latex.py
 # reads book-in.tex -> writes book-in2.tex
 
 # remove unicode chars via unix iconv
